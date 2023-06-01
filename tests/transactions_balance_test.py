2083eb8c210ff5400603584153a77d8b8724ad7a
Matěj Hampl <matej.hampl@tul.cz>
2023-06-01 12:42:11 +0200

    Update transactions_balance_test.py

--- tests/transactions_balance_test.py
+++ tests/transactions_balance_test.py
@@ -30,7 +30,7 @@ def test_update_balance(tmp_path, monkeypatch):
     msg, currency, amount = update_balance(user_email, -50, "USD", balance_file)
     with open(balance_file, "r") as file:
         all_balances = json.load(file)
-    assert all_balances[user_email]["USD"] == 50
+    assert all_balances[user_email]["USD"] == 55
     assert msg == ""
     assert currency == "USD"
     assert amount == -50
